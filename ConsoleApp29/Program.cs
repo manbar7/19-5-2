96eb338885e101df52eda76f1eb3113587e14a40
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-05-26 20:33:39 +0300

    Add files via upload

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        public static void PrintWhichDay(int Day)
+        {
+
+            Enum.TryParse($"{Day}", out WhichDay result);
+            Console.WriteLine(result);
+            
+        }
+
+        public static void WhichDayNumber(WhichDay DayName)
+        {
+            // Enum.TryParse($"{DayName}", out WhichDay result);
+            // Console.WriteLine(result);
+            Console.WriteLine((int)DayName);
+
+        }
+        static void Main(string[] args)
+        {
+            Enum.GetNames(typeof(WhichDay)).ToList().ForEach(d => Console.WriteLine(d));
+            Console.WriteLine("------------------------------");
+
+            int day = 3;
+            string DayName = "Tuesday";
+            PrintWhichDay(day);
+            Console.WriteLine("----------------");
+            WhichDayNumber(WhichDay.Friday);
+
+        }
+
+        
+
+        
+    }
+}
---
+++ ConsoleApp29/WhichDay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+        public enum WhichDay
+        {
+            Sunday = 1,
+            Monday = 2,
+            Tuesday = 3,
+            Wedensday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7
+        }
+            
+}
